ce7ad6902b8746453f4a3024465b0f67f067eb62
Moss <moss@mossx.net>
2022-10-04 00:42:22 -0400

    AES: Added Galois Field Functions

--- main.cpp
+++ main.cpp
@@ -162,10 +162,11 @@ void sha1(const uint32_t* data, int data_size_bytes, uint32_t* output) {
         chunks.back()[i % 16] = message[i];
     }
 
-//    for (int i = 0; i < 16; i++) {
-//        std::cout << std::hex << chunks[0][i] << ' ';
+//    for (auto& chunk : chunks) {
+//        for (int i = 0; i < 16; i++) {
+//            std::cout << chunk[i] << '\n';
+//        }
 //    }
-//    std::cout << '\n';
 
     for (auto& chunk : chunks) {
         // break chunk into sixteen 32-bit big-endian words w[i], 0 ≤ i ≤ 15
@@ -452,29 +453,226 @@ std::string pbkdf2(const std::string& password, std::string salt, int iterations
 }
 
 
-int main() {
-
-    std::string password = "peanuts";
-    int iterations = 1;
-    std::string salt = "saltysalt";
-    int length = 16;
 
 
 
-    uint8_t key[32];
+std::vector<uint32_t> aes_get_round_constants(uint8_t rounds) {
 
-    char output[128];
+    auto *rc = new uint8_t[rounds + 1];
 
-    char iv[16];
-
-    for (char& c : iv) {
-    c = ' ';
+    for (int i = 1; i <= rounds; i++) {
+        if (i == 1) {
+            rc[i] = 1;
+        } else if (rc[i - 1] < 0x80) {
+            rc[i] = 2 * rc[i - 1];
+        } else {
+            rc[i] = (2 * rc[i - 1]) ^ 0x11b;
+        }
     }
 
-    for (char& c : output) {
-    c = 0;
+    std::vector<uint32_t> out(1);
+    for (int i = 1; i <= rounds; i++) {
+        out.push_back(rc[i] << 24);
     }
 
+
+    return out;
+}
+
+
+uint32_t aes_rot_word(uint32_t word) {
+    return rotateleft(word, 8);
+}
+
+/*
+aes affine transformation individual values
+{{1, 0, 0, 0, 1, 1, 1, 1},
+{1, 1, 0, 0, 0, 1, 1, 1},
+{1, 1, 1, 0, 0, 0, 1, 1},
+{1, 1, 1, 1, 0, 0, 0, 1},
+{1, 1, 1, 1, 1, 0, 0, 0},
+{0, 1, 1, 1, 1, 1, 0, 0},
+{0, 0, 1, 1, 1, 1, 1, 0},
+{0, 0, 0, 1, 1, 1, 1, 1}}
+ */
+
+/*
+ *
+aes affine transformation combined values
+ {0b10001111,
+0b11000111,
+0b11100011,
+0b11110001,
+0b11111000,
+0b01111100,
+0b00111110,
+0b00011111}
+ */
+
+std::vector<std::vector<uint8_t>> sbox_matrix = {{1, 0, 0, 0, 1, 1, 1, 1},
+                                                {1, 1, 0, 0, 0, 1, 1, 1},
+                                                {1, 1, 1, 0, 0, 0, 1, 1},
+                                                {1, 1, 1, 1, 0, 0, 0, 1},
+                                                {1, 1, 1, 1, 1, 0, 0, 0},
+                                                {0, 1, 1, 1, 1, 1, 0, 0},
+                                                {0, 0, 1, 1, 1, 1, 1, 0},
+                                                {0, 0, 0, 1, 1, 1, 1, 1}};
+
+uint8_t sbox_vector[] = {1, 1, 0, 0, 0, 1, 1, 0};
+
+uint8_t* sbox = nullptr;
+
+const uint16_t AES_IRREDUCIBLE_POLYNOMIAL = 0b100011011;
+
+//const uint8_t sbox_const[] = {0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+//           0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+//           0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+//           0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+//           0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+//           0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+//           0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+//           0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+//           0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+//           0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+//           0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+//           0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+//           0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+//           0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+//           0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+//           0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16};
+
+
+
+uint8_t gf2_8_multiplication(uint8_t a, uint8_t b, uint16_t polynomial) {
+    uint8_t polynomial_degree = 0; uint16_t polynomial_copy = polynomial, polynomial_leading_coefficient;
+    for (; polynomial_copy >> (++polynomial_degree + 1););
+    polynomial_leading_coefficient = 1 << polynomial_degree;
+
+
+    uint16_t output = 0;
+    uint16_t b_copy = b;
+
+    uint8_t a_bits[8];
+
+    for (int i = 0; i < 8; i++) {
+        a_bits[i] = (a >> i) & (0b1);
+    }
+
+    for (int i = 0; i < 8; i++) {
+        if (a_bits[i]) {
+            output ^= b_copy << i;
+        }
+    }
+
+    if (output < polynomial_leading_coefficient)
+        return output;
+
+
+    while (output >= polynomial_leading_coefficient) {
+        uint8_t output_degree = 0;
+        uint16_t output_copy = output; polynomial_copy = polynomial;
+        for (; output_copy >> (++output_degree + 1););
+
+        uint8_t degree_difference = output_degree - polynomial_degree;
+
+        uint8_t output_bits[16];
+
+        for (int i = 0; i < 16; i++) {
+            output_bits[i] = (output >> i) & (0b1);
+        }
+
+        polynomial_copy = polynomial_copy << degree_difference;
+
+        output ^= polynomial_copy;
+    }
+
+    return output;
+}
+
+/**
+ *
+ * @param a
+ * @param b
+ * @return A 16-bit value which is comprised of the quotient in the first 8-bits and the remainder in the last 8-bits
+ */
+uint16_t gf2_8_division(uint16_t a, uint16_t b) {
+    if (b == 0) {
+        std::cerr << "DIV ERROR: Divide by zero\n";
+        exit(1);
+    }
+
+    uint8_t b_degree = 0; uint16_t b_copy = b, b_leading_coefficient;
+    for (; b_copy >> (b_degree + 1); b_degree++);
+    b_leading_coefficient = 1 << b_degree;
+
+    uint8_t quotient = 0;
+
+    while (a >= b_leading_coefficient) {
+        uint8_t a_degree = 0;
+        b_degree = 0;
+        uint16_t a_copy = a;
+        b_copy = b;
+        for (; a_copy >> (a_degree + 1); a_degree++);
+        for (; b_copy >> (b_degree + 1); b_degree++);
+
+        uint8_t degree_difference = a_degree - b_degree;
+
+        b_copy = b_copy << degree_difference;
+
+        quotient |=  0b1 << degree_difference;
+
+        a ^= b_copy;
+    }
+
+    return ((quotient << 8) | a);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *
+ * @param value - Any value on the fininte field GF(2^8)
+ * @param polynomial - any irreducible polynomial in binary (AES uses x^8 + x^4 + x^3 + x + 1 or 0b100011011)
+ * @return The <b>value</b>'s inverse
+ *
+ * Uses the Extended Euclidean algorithm
+ */
+uint8_t gf_2_8_get_value_inverse(const uint8_t value, uint16_t polynomial) {
+
+    std::vector<uint16_t> remainders = {polynomial, value};
+    std::vector<uint16_t> quotients = {0};
+
+    uint16_t first_result = gf2_8_division(polynomial, value);
+
+    quotients.push_back((first_result >> 8) & 0xff);
+    remainders.push_back(first_result & 0xff);
+
+    for (int i = 2; remainders.back(); i++) {
+        uint16_t result = gf2_8_division(remainders[i-1], remainders[i]);
+
+        quotients.push_back((result >> 8) & 0xff);
+        remainders.push_back(result & 0xff);
+    }
+
+    uint8_t aux[quotients.size() + 1];
+
+    aux[0] = 0; aux[1] = 1;
+
+    for (int i = 2; i < quotients.size() + 1; i++) {
+        aux[i] = aux[i-2] ^ gf2_8_multiplication(quotients[i-1], aux[i-1], polynomial);
+    }
+
+    return aux[quotients.size() - 1];
+}
 
     auto *sha = new uint32_t[5];
     auto *sha2 = new uint32_t[5];
