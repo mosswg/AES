84922517831360c8bcb932e10008ed811320e7e9
Moss <moss@mossx.net>
2022-10-04 00:01:01 -0400

    Helpers: Added Helper Functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <climits>
 #include <vector>
+#include <sstream>
+#include <cmath>
+#include <bitset>
 
 /// Source: https://en.wikipedia.org/wiki/Circular_shift#Implementing_circular_shifts
 uint32_t rotateleft (uint32_t value, unsigned int count) {
@@ -14,6 +17,15 @@ uint32_t rotateright (uint32_t value, unsigned int count) {
     const unsigned int mask = CHAR_BIT * sizeof(value) - 1;
     count &= mask;
     return (value >> count) | (value << (-count & mask));
+}
+
+void print_uint_bytes(uint32_t word) {
+    uint32_t b0 = ((word >> 24) & 0xff);
+    uint32_t b1 = ((word >> 16) & 0xff);
+    uint32_t b2 = ((word >> 8) & 0xff);
+    uint32_t b3 = (word & 0xff);
+
+    std::cout << std::hex << ((b0 < 0x10) ? "0" : "") << b0 << ((b1 < 0x10) ? " 0" : " ") << b1 << ((b2 < 0x10) ? " 0" : " ") << b2 << ((b3 < 0x10) ? " 0" : " ") << b3 << std::dec;
 }
 
 void convert_be(const std::string& data, uint32_t* output, uint32_t bit_limit = 0) {
@@ -31,6 +43,37 @@ void convert_be(const std::string& data, uint32_t* output, uint32_t bit_limit = 
 
         bits += 8;
     }
+}
+
+std::vector<uint32_t> convert_be(const std::string& data) {
+    int bits = 0;
+
+    std::vector<uint32_t> out;
+
+    for (char ch : data) {
+        if (bits % 32 == 0) {
+            out.push_back(0);
+        }
+
+        out[bits / 32] |= ((ch << (24 - (bits % 32))) & (0xFF << (24 - (bits % 32))));
+
+        bits += 8;
+    }
+
+    return out;
+}
+
+std::string convert_be(uint32_t* data, uint32_t data_size) {
+    std::string out;
+
+    for (int i = 0; i < data_size / 4; i++) {
+        out += ((data[i] & 0xFF000000) >> 24);
+        out += ((data[i] & 0xFF0000) >> 16);
+        out += ((data[i] & 0xFF00) >> 8);
+        out += (data[i] & 0xFF);
+    }
+
+    return out;
 }
 
 
@@ -57,136 +100,6 @@ void set_byte_in_uint_array(uint32_t* data, int byte_index, uint8_t byte) {
     data[uint_index] &= mask;
     data[uint_index] |= byte << ((3-sub_byte_index) * 8);
 }
-
-/// Source: https://en.wikipedia.org/wiki/SHA-1#SHA-1_pseudocode
-void sha1(std::string data, uint32_t* output) {
-
-
-    // Note 1: All variables are unsigned 32-bit quantities and wrap modulo 232 when calculating, except for
-    //      ml, the message length, which is a 64-bit quantity, and
-    //      hh, the message digest, which is a 160-bit quantity.
-    // Note 2: All constants in this pseudo code are in big endian.
-    //      Within each word, the most significant byte is stored in the leftmost byte position
-
-    // Initialize variables:
-
-    uint32_t h0 = 0x67452301;
-    uint32_t h1 = 0xEFCDAB89;
-    uint32_t h2 = 0x98BADCFE;
-    uint32_t h3 = 0x10325476;
-    uint32_t h4 = 0xC3D2E1F0;
-
-    uint64_t ml = data.size() * 8;
-
-    // Pre-processing:
-    // append the bit '1' to the message e.g. by adding 0x80 if message length is a multiple of 8 bits.
-    //        append 0 ≤ k < 512 bits '0', such that the resulting message length in bits
-    // is congruent to −64 ≡ 448 (mod 512)
-    // append ml, the original message length in bits, as a 64-bit big-endian integer.
-    //        Thus, the total length is a multiple of 512 bits.
-
-    data += (char)0b10000000;
-
-    while ((data.size() * 8) % 512 != 448) {
-        data += (char)0;
-    }
-    data += (char)((ml & 0xFF00000000000000) >> 56);
-    data += (char)((ml & 0x00FF000000000000) >> 48);
-    data += (char)((ml & 0x0000FF0000000000) >> 40);
-    data += (char)((ml & 0x000000FF00000000) >> 32);
-    data += (char)((ml & 0xFF000000) >> 24);
-    data += (char)((ml & 0x00FF0000) >> 16);
-    data += (char)((ml & 0x0000FF00) >> 8);
-    data += (char)(ml & 0x000000FF);
-
-    // Process the message in successive 512-bit chunks:
-    // break message into 512-bit chunks
-
-    std::vector<uint32_t*> chunks;
-
-    int bits = 0;
-    for (unsigned char ch : data) {
-        if (bits % 512 == 0) {
-            bits = 0;
-            chunks.push_back(new uint32_t[80]);
-        }
-
-//        if (bits % 32 == 0) {
-//            chunks.back().push_back(0);
-//        }
-
-        chunks.back()[bits / 32] |= ((ch << (24 - (bits % 32))) & (0xFF << (24 - (bits % 32))));
-
-        bits += 8;
-    }
-
-//    for (int i = 0; i < 16; i++) {
-//        std::cout << std::hex << chunks[0][i] << ' ';
-//    }
-//    std::cout << '\n';
-
-    for (auto& chunk : chunks) {
-        // break chunk into sixteen 32-bit big-endian words w[i], 0 ≤ i ≤ 15
-
-        // Message schedule: extend the sixteen 32-bit words into eighty 32-bit words:
-        // for i from 16 to 79
-        // Note 3: SHA-0 differs by not having this leftrotate.
-        for (int i = 16; i < 80; i++) {
-            chunk[i] = rotateleft((chunk[i - 3] ^ chunk[i - 8] ^ chunk[i - 14] ^ chunk[i - 16]), 1);
-        }
-
-        // Initialize hash value for this chunk:
-        uint32_t a = h0;
-        uint32_t b = h1;
-        uint32_t c = h2;
-        uint32_t d = h3;
-        uint32_t e = h4;
-
-        //  Main loop:[10][56]
-        for (int i = 0; i < 80; i++) {
-            uint32_t f;
-            uint32_t k;
-            if (0 <= i && i <= 19) {
-                f = (b & c) | ((~b) & d);
-                k = 0x5A827999;
-            } else if (i >= 20 && i <= 39) {
-                f = b ^ c ^ d;
-                k = 0x6ED9EBA1;
-            } else if (i >= 40 && i <= 59) {
-                f = (b & c) | (b & d) | (c & d);
-                k = 0x8F1BBCDC;
-            } else {
-                f = b xor c xor d;
-                k = 0xCA62C1D6;
-            }
-
-            uint32_t temp = (rotateleft(a, 5)) + f + e + k + chunk[i];
-            e = d;
-            d = c;
-            c = rotateleft(b, 30);
-            b = a;
-            a = temp;
-        }
-
-         // Add this chunk's hash to result so far:
-        h0 = h0 + a;
-        h1 = h1 + b;
-        h2 = h2 + c;
-        h3 = h3 + d;
-        h4 = h4 + e;
-    }
-
-    for (auto& chunk : chunks) {
-        delete[] chunk;
-    }
-
-    output[0] = h0;
-    output[1] = h1;
-    output[2] = h2;
-    output[3] = h3;
-    output[4] = h4;
-}
-
 
 /// Source: https://en.wikipedia.org/wiki/SHA-1#SHA-1_pseudocode
 void sha1(const uint32_t* data, int data_size_bytes, uint32_t* output) {
